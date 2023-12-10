ab59a76fab1232931403e456d9d104563eeb030a
MercyP30 <munzenzimercy9@gmail.com>
2023-12-11 03:51:08 +0700

    Lab 12 done

---
+++ Backend2/Models/FormData.cs
@@ -0,0 +1,9 @@
+﻿namespace Backend2.Models
+{
+    public class FormData
+    {
+        public int firstValue { get; set; }
+        public int secondValue { get; set; }
+        public string operation { get; set; }
+    }
+}
